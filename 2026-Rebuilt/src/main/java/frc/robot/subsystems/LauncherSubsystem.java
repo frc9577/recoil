d58bef5152f7161f5e778972ed123239366934bf
Dave Wilson <dave@webartz.com>
2026-02-14 09:45:05 -0600

    Add skeleton for Launcher subsystem class interface.

--- 2026-Rebuilt/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ 2026-Rebuilt/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -8,9 +8,66 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LauncherConstants;
 
 public class LauncherSubsystem extends SubsystemBase {
+  private double m_targetSpeedrpm;
+  private boolean m_liftRunning;
+
   /** Creates a new LauncherSubsystem. */
   public LauncherSubsystem() {
     // TODO: write this
+  }
+
+  //
+  // Set the speed of the launcher flywheel in revolutions per minute.
+  //
+  public void setTargetSpeedrpm(double RPM)
+  {
+    m_targetSpeedrpm = RPM;
+
+    // TODO: Populate this!
+  }
+
+  //
+  // Get the current launcher flywheel speed in revolutions per minute.
+  //
+  public double getCurrentSpeedrpm()
+  {
+    // TODO: Populate this!
+    return 0.0;
+  }
+
+  //
+  // Get the current target speed for the launcher flywheel in revolutions
+  // per minute.
+  //
+  public double getTargetSpeedrpm()
+  {
+    return m_targetSpeedrpm;
+  }
+
+  //
+  // Start the lift mechanism motor.
+  //
+  public void startLift()
+  {
+    // TODO: Populate this!
+  }
+
+  //
+  // Stop the lift mechanism motor.
+  //
+  public void stopLift()
+  {
+    // TODO: Populate this!
+  }
+
+  //
+  // Determine whether or not a fuel is in position beneath the launcher
+  // entrance.
+  //
+  public boolean isFuelAtLauncher()
+  {
+    // TODO: Populate this!
+    return false;
   }
 
   @Override
